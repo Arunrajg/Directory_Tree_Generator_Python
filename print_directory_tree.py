28c4a39059313e67a160aec51b565600c17d76e6
Arun Raj <arungraj23@gmail.com>
2022-10-26 22:16:07 +0530

    Updated logic to print file extensions

--- print_directory_tree.py
+++ print_directory_tree.py
@@ -15,12 +15,13 @@ def default_criteria(path: Path):
 def make_tree(root: str, level: int = -1, directories_only: bool = False, criteria: object = None):
     files = 0
     directories = 0
+    file_extension = {}
     root = Path(str(root))
     if not criteria:
         criteria = default_criteria
 
     def inner(root: Path, level: int = level, prefix: str = ""):
-        nonlocal files, directories
+        nonlocal files, directories, file_extension
         if not level:
             return
         if directories_only:
@@ -38,9 +39,16 @@ def make_tree(root: str, level: int = -1, directories_only: bool = False, criter
                 yield from inner(content, level=level-1, prefix=prefix+extension)
             elif not directories_only:
                 files += 1
-                yield prefix+pointer+content.name
-    tree = [root.name+"/"] + list(inner(root, level)) + \
-        [f"{directories} directories, {files} files"]
+                file_suffix = content.suffix
+                if not file_suffix:
+                    file_suffix = "no_extension"
+                if file_suffix in file_extension:
+                    file_extension[file_suffix] += 1
+                else:
+                    file_extension[file_suffix] = 1
+                yield prefix + pointer + content.name
+    tree = [root.name+"/"] + list(inner(root, level)) + [f"Total {directories} directories, {files} files"] + ["\n"] + [
+        f"{ext} = {num} files" for ext, num in file_extension.items()]
     return tree
 
 
@@ -59,4 +67,11 @@ def print_tree(input_folder: str, level=-1, directory_only=False):
 
 if __name__ == "__main__":
     input_path = input("Enter Input Folder: ")
-    print_tree(input_path)
+    nested_level = input("Enter num of levels(Default:All): ")
+    directories_only = input("Do you want to print only directories(y/n)? ")
+    try:
+        level = int(nested_level)
+    except Exception:
+        level = -1
+    directory_only = directories_only.lower() == 'y'
+    print_tree(input_path, level, directory_only)
